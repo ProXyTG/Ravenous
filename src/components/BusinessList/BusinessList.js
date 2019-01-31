6498ab99e25cccc539d9a4c80422ce3e3414ca70
ProXyTG <proxytg1@gmail.com>
2019-01-31 02:28:50 +0100

    file

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './BusinessList.css';
-
 import Business from '../Business/Business';
 
-class BusinessList extends React.Component {
-  render() {
-    return (
+const BusinessList = ({businesses}) => {
+    let renderBussinesses = (
       <div className="BusinessList">
-        {
-          this.props.businesses.map(business => {
-            return <Business key={business.id} business={business} />
-          })
-        }
       </div>
     );
-  }
-}
+    if (businesses === undefined) {
+      renderBussinesses = (
+        <div className="BusinessList">
+        <h2>No results found.</h2>
+        </div>
+      );
+    } else if (businesses.length === 0) {
+      renderBussinesses = (
+        <div className="BusinessList">
+          <h2>No results found.</h2>
+        </div>
+      );
+    } else {
+      renderBussinesses = (
+        <div className="BusinessList">
+          {businesses.map(business => {
+              return <Business key={business.id} business={business} />
+          })};
+        </div>
+      );
+    }
+    return renderBussinesses;
+};
 
 export default BusinessList;
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,30 +1,29 @@
-const apiKey='w86SMysLCHC7cBrwZwZMzDsCHBqWDEbecGEx9jNAfuS7IDdcbQxqnUbaXXaO4ZdYEjQ5ePRByYcWz5nApicyvDn3E3y2aK8bpezTl1j9shB24PzFELVp8rXT-F9PXHYx';
+const apiKey = 'w86SMysLCHC7cBrwZwZMzDsCHBqWDEbecGEx9jNAfuS7IDdcbQxqnUbaXXaO4ZdYEjQ5ePRByYcWz5nApicyvDn3E3y2aK8bpezTl1j9shB24PzFELVp8rXT-F9PXHYx'
 
 const Yelp = {
-  searchYelp(term, location, sortBy) {
+  search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    }).then((response) => {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
       return response.json();
-    }).then((jsonResponse) => {
+    }).then(jsonResponse => {
       if (jsonResponse.businesses) {
-        return jsonResponse.businesses.map(((business) => {
-          console.log(business);
+        return jsonResponse.businesses.map(business => {
           return {
-            id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            address: business.location.address1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories[0].title,
-            rating: business.rating,
-            reviewCount: business.review_count,
-          };
-        }));
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+          }
+        })
       }
     })
   }
